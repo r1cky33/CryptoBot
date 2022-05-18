8bce12f9769fa7da4e7b69bdf12e9849eb595e24
r1cky33 <49491951+r1cky33@users.noreply.github.com>
2022-05-18 21:27:11 +0200

    censored

--- cbot/cfg.py
+++ cbot/cfg.py
@@ -1,16 +1,16 @@
 # bybitapi
-api_key         = 'DFGLALPKOFDRMGYPHA'
-api_secret      = 'HPKVKLVXQIQRLKQDSICEZCRGBHWVCAOTXQAK'
+api_key         = '{censored}'
+api_secret      = '{censored}'
 
 # bybit endpoint
 bybit_endpoint  = f'https://api.bybit.com'
 
 # taapi
-taapi_key       = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InIuaGFja2xAcHJvdG9ubWFpbC5jb20iLCJpYXQiOjE2NDk3ODcwMzEsImV4cCI6Nzk1Njk4NzAzMX0.LWPpR9iwer4w4eSO0DEj6dBjEG0lkuE7vd0gCkZIJyk'
+taapi_key       = '{censored}'
 taapi_endpoint  = f'https://api.taapi.io/atr'
 
 # tv strategy_secret
-strategy_secret = 'Xw72xKxPTTiQ1zacrjsJ'
+strategy_secret = '{censored}'
 
 # position settings
 qty15m = 0
